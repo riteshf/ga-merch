eb1a8596798cd7c239456b69a0056d4bb2b04cf3
Ritesh Firodiya <ritesh.firodiya@gmail.com>
2023-01-13 23:14:19 +0530

    expoted props

--- src/redux/cart/cart.api.ts
+++ src/redux/cart/cart.api.ts
@@ -3,14 +3,14 @@ import axios, { AxiosResponse } from "axios";
 
 export const getCartAPI = async () => {
   let response: AxiosResponse<Cart[]> = await axios.get(
-    `${process.env.BACKEND_URL}/carts`
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/carts`
   );
   return response.data;
 };
 
 export const addItemToCartAPI = async (productId: number, quantity: number) => {
   let response: AxiosResponse<Cart> = await axios.post(
-    `${process.env.BACKEND_URL}/carts`,
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/carts`,
     { productId, quantity }
   );
   return response.data;
--- src/redux/product/product.api.ts
+++ src/redux/product/product.api.ts
@@ -3,14 +3,14 @@ import axios, { AxiosResponse } from "axios";
 
 export const getProductsAPI = async () => {
   let response: AxiosResponse<Product[]> = await axios.get(
-    `${process.env.BACKEND_URL}/products`
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/products`
   );
   return response.data;
 };
 
 export const getProductAPI = async (id: string) => {
   let response: AxiosResponse<Product[]> = await axios.get(
-    `${process.env.BACKEND_URL}/products/${id}`
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/products/${id}`
   );
   return response.data;
 };
